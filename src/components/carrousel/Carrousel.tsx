c2b2536e33022ae080ab8ff9c22c4e21ec406485
Maicon Nunes <mnunes315@hotmail.com>
2023-03-20 22:46:54 -0300

    Outras partes do carrousel

--- src/components/carrousel/Carrousel.tsx
+++ src/components/carrousel/Carrousel.tsx
@@ -1,6 +1,7 @@
 import { CaretLeft, CaretRight } from "phosphor-react"
 import { useState } from "react"
 import { GrNext, GrPrevious } from "react-icons/gr"
+import {MdOutlineArrowForwardIos, MdArrowForwardIos} from "react-icons/md"
 import "./carrousel.scss"
 
 type CarrouselPropsI = {
@@ -16,8 +17,25 @@ export default function Carrousel({category,image,id}:CarrouselPropsI) {
 
   return (
     <div className="carrousel" >
-      <h2>{category}</h2>
+      <div className="category-container">
 
+        <div className="category-left">
+        <h2>{category}</h2>
+        <span className="hidden">Ver tudo </span>
+        <CaretRight weight="bold" className="arrow hidden" />
+        </div>
+
+        <div className="category-right">
+          <div className="item active"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+          <div className="item"></div>
+        </div>
+
+      </div>
       <div className="div-imgs" >
     
       <button id={id} className="button-carrousel-prev" onClick={() => {
@@ -41,9 +59,32 @@ export default function Carrousel({category,image,id}:CarrouselPropsI) {
 
         <div className="container-img">
         {image.map((img,index) => (
-          <div key={index} >
+          <div key={index} className="">
             
-            <img className="img-carrousel" src={ `https://image.tmdb.org/t/p/original${img}`} />
+            <img className="img-carrousel" src={`https://image.tmdb.org/t/p/original${img}`} />
+            
+            <div>
+              <div>
+                <div>
+                  <span>Play</span>
+                  <span>Adicionar na lista</span>
+                  <span>gostei</span>
+                </div>
+                <span>episódios e informações</span>
+              </div>
+
+              <div>
+                <span>relevancia</span>
+                <span>classificação indicativa</span>
+                <span>quant. episodios</span>
+              </div>
+              
+              <div>
+                <span>cat1</span>
+                <span>cat2</span>
+                <span>cat3</span>
+              </div>
+            </div>
          </div>
         ))}  
         </div>  
